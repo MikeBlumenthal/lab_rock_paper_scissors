1532df9805d8b66a4229f067063327ce018f40bc
Michael Blumenthal <bloomer25@googlemail.com>
2018-08-21 22:52:10 +0100

    adds views

--- controller.rb
+++ controller.rb
@@ -4,9 +4,17 @@ require_relative('./models/game')
 also_reload('./models/*')
 
 get '/' do
+  erb(:home)
 end
 
 get '/play/:hand1/:hand2' do
   game = Game.new(params[:hand1].to_s, params[:hand2].to_s)
-  return "#{game.winner.capitalize} wins!"
+  if game.draw?
+    erb(:draw)
+  else
+    @winner = game.winner
+    @loser = game.loser
+    @hand = game.winning_hand.capitalize
+    erb(:result)
+  end
 end
--- models/game.rb
+++ models/game.rb
@@ -3,6 +3,8 @@ class Game
  attr_reader :hand1, :hand2
 
   def initialize(hand1, hand2)
+    @player1 = "Player 1"
+    @player2 = "Player 2"
     @hand1 = hand1
     @hand2 = hand2
   end
@@ -39,18 +41,34 @@ class Game
     end
   end
 
+  def draw?
+    if self.play? == nil
+      return true
+    end
+  end
+
   def winner
     result = self.play?
-    return @hand1 if result == true
-    return @hand2 if result == false
-    return "Nobody" if result == nil
+    return @player1 if result == true
+    return @player2 if result == false
   end
 
   def loser
     result = self.play?
+    return @player2 if result == true
+    return @player1 if result == false
+  end
+
+  def winning_hand
+    result = self.play?
+    return @hand1 if result == true
+    return @hand2 if result == false
+  end
+
+  def losing_hand
+    result = self.play?
     return @hand2 if result == true
     return @hand1 if result == false
-    return "Nobody" if result == nil
   end
 
 end
